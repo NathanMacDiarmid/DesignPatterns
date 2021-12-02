41d18e274aec3b17c275c1a60bb22b5cad2797fe
Nathan <nathanmacdiarmid@gmail.com>
2021-12-02 14:26:08 -0500

    Added and finished Lab9

---
+++ Lab9/src/main/java/AddressBook.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class AddressBook extends DefaultListModel<BuddyInfo> implements Serializable {
+
+    public AddressBook() {
+    }
+    public void addBuddy(BuddyInfo buddy) {
+        addElement(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        removeElement(buddy);
+    }
+
+    public void save(String filename) {
+        try {
+            Writer w = new FileWriter(filename);
+            String s = "";
+            for (int i = 0; i < size(); i++) {
+                if (i == size() - 1) {
+                    s += get(i).toString();
+                }
+                else {
+                    s += get(i).toString() + "\n";
+                }
+            }
+            w.write(s);
+            w.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void importAddressBook(String filename) {
+        try {
+            Scanner fileText = new Scanner(new File(filename));
+            while (fileText.hasNextLine()) {
+                addBuddy(BuddyInfo.importBuddyInfo(fileText.nextLine()));
+            }
+            fileText.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        AddressBook book = new AddressBook();
+        BuddyInfo b1 = new BuddyInfo();
+        BuddyInfo b2 = new BuddyInfo();
+        b2.setAddress("Miramichi NB");
+        b2.setBuddy("Nathan");
+        b2.setNumber("123456789");
+        book.addBuddy(b1);
+        book.addBuddy(b2);
+
+        book.save("output.txt");
+
+        book.save("output.txt");
+
+        AddressBook book2 = new AddressBook();
+        book2.importAddressBook("output.txt");
+        for (int i = 0; i < book2.size(); i++) {
+            System.out.println(book2.get(i).toString());
+        }
+
+    }
+}
